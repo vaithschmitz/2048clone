55722d52339a339d13e536343ca48724b6748825
vaithschmitz <vaithschmitz@googlemail.com>
2020-06-28 19:54:31 +0100

    separating utils

--- App.js
+++ App.js
@@ -1,123 +1,10 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground ,Dimensions, Vibration, Alert, Modal } from 'react-native';
 import {Directions, FlingGestureHandler, State} from 'react-native-gesture-handler';
+import {chooseMonster, images} from './utils'
 import Constants from 'expo-constants';
 import sky from './assets/sky.jpg'
 
-const images = {
-  monsters:{
-    Transparent:{
-      file: require('./assets/monsters/transparent.png')
-    },
-    One: {
-      file: require('./assets/monsters/lvl1.png')
-    },
-    Two:{
-      file: require('./assets/monsters/lvl2.png')
-    },
-    Three:{
-      file: require('./assets/monsters/lvl3.png')
-    },
-    Four:{
-      file: require('./assets/monsters/lvl4.png')
-    },
-    Five:{
-      file: require('./assets/monsters/lvl5.png')
-    },
-    Six:{
-      file: require('./assets/monsters/lvl6.png')
-    },
-    Seven:{
-      file: require('./assets/monsters/lvl7.png')
-    },
-    Eight:{
-      file: require('./assets/monsters/lvl8.png')
-    },
-    Nine:{
-      file: require('./assets/monsters/lvl9.png')
-    },
-    Ten:{
-      file: require('./assets/monsters/lvl10.png')
-    },
-    Eleven:{
-      file: require('./assets/monsters/lvl11.png')
-    },
-    Twelve:{
-      file: require('./assets/monsters/lvl12.png')
-    },
-    Thirteen:{
-      file: require('./assets/monsters/lvl13.png')
-    },
-    Fourteen:{
-      file: require('./assets/monsters/lvl14.png')
-    },
-    Fifteen:{
-      file: require('./assets/monsters/lvl15.png')
-    },
-    Sixteen:{
-      file: require('./assets/monsters/lvl16.png')
-    },
-    Seventeen:{
-      file: require('./assets/monsters/lvl17.png')
-    }
-  },
-  numbers:{
-    Transparent:{
-      file: require('./assets/monsters/transparent.png')
-    },
-    One: {
-      file: require('./assets/numbers/2.png')
-    },
-    Two:{
-      file: require('./assets/numbers/4.png')
-    },
-    Three:{
-      file: require('./assets/numbers/8.png')
-    },
-    Four:{
-      file: require('./assets/numbers/16.png')
-    },
-    Five:{
-      file: require('./assets/numbers/32.png')
-    },
-    Six:{
-      file: require('./assets/numbers/64.png')
-    },
-    Seven:{
-      file: require('./assets/numbers/128.png')
-    },
-    Eight:{
-      file: require('./assets/numbers/256.png')
-    },
-    Nine:{
-      file: require('./assets/numbers/512.png')
-    },
-    Ten:{
-      file: require('./assets/numbers/1024.png')
-    },
-    Eleven:{
-      file: require('./assets/numbers/4096.png')
-    },
-    Twelve:{
-      file: require('./assets/numbers/2048.png')
-    },
-    Thirteen:{
-      file: require('./assets/numbers/8192.png')
-    },
-    Fourteen:{
-      file: require('./assets/numbers/16384.png')
-    },
-    Fifteen:{
-      file: require('./assets/numbers/32768.png')
-    },
-    Sixteen:{
-      file: require('./assets/numbers/65536.png')
-    },
-    Seventeen:{
-      file: require('./assets/numbers/131072.png')
-    }
-  }
-}
 
 export default function App() {
   const startingBoard = [['','', '',''],['','',2,''],['','',2,''],['','','','']]
@@ -303,64 +190,7 @@ export default function App() {
     setBoard(startingBoard)
   }
 
-  const chooseMonster = (x,y) => {
-    console.log(board[x][y])
-    if(board[x][y] == ""){
-      return images[theme]['Transparent'].file
-    }
-    if(board[x][y] == 2){
-      return images[theme]['One'].file
-    }
-    if(board[x][y] == 4){
-      return images[theme]['Two'].file
-    }    
-    if(board[x][y] == 8){
-      return images[theme]['Three'].file
-    }
-    if(board[x][y] == 16){
-      return images[theme]['Four'].file
-    }
-    if(board[x][y] == 32){
-      return images[theme]['Five'].file
-    }
-    if(board[x][y] == 64){
-      return images[theme]['Six'].file
-    }
-    if(board[x][y] == 128){
-      return images[theme]['Seven'].file
-    }
-    if(board[x][y] == 256){
-      return images[theme]['Eight'].file
-    }
-    if(board[x][y] == 512){
-      return images[theme]['Nine'].file
-    }
-    if(board[x][y] == 1024){
-      return images[theme]['Ten'].file
-    }
-    if(board[x][y] == 2048){
-      return images[theme]['Eleven'].file
-    }
-    if(board[x][y] == 4096){
-      return images[theme]['Twelve'].file
-    }
-    if(board[x][y] == 8192){
-      return images[theme]['Thirteen'].file
-    }
-    if(board[x][y] == 16384){
-      return images[theme]['Fourteen'].file
-    }
-    if(board[x][y] == 32768){
-      return images[theme]['Fifteen'].file
-    }
-    if(board[x][y] == 65536){
-      return images[theme]['Sixteen'].file
-    }
-    if(board[x][y] == 131072){
-      return images[theme]['Seventeen'].file
-    }
-  }
-
+ 
   return (
     <View style={styles.screen}>
     <ImageBackground source={sky} style={styles.bg}>
@@ -462,28 +292,28 @@ export default function App() {
             </View>
           <View style={styles.gridContainer}>
             <View>      
-              <View style={[styles.cell, styles.cell1]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(0,0)}></Image></View>
-              <View style={[styles.cell, styles.cell2]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(0,1)}></Image></View>
-              <View style={[styles.cell, styles.cell3]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(0,2)}></Image></View>
-              <View style={[styles.cell, styles.cell4]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(0,3)}></Image></View>
+              <View style={[styles.cell, styles.cell1]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(0,0, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell2]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(0,1, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell3]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(0,2, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell4]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(0,3, board, theme)}></Image></View>
             </View>
             <View>
-            <View style={[styles.cell, styles.cell5]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(1,0)}></Image></View>
-              <View style={[styles.cell, styles.cell6]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(1,1)}></Image></View>
-              <View style={[styles.cell, styles.cell7]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(1,2)}></Image></View>
-              <View style={[styles.cell, styles.cell8]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(1,3)}></Image></View>
+            <View style={[styles.cell, styles.cell5]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(1,0, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell6]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(1,1, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell7]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(1,2, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell8]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(1,3, board, theme)}></Image></View>
             </View>
             <View>
-            <View style={[styles.cell, styles.cell9]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(2,0)}></Image></View>
-              <View style={[styles.cell, styles.cell10]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(2,1)}></Image></View>
-              <View style={[styles.cell, styles.cell11]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(2,2)}></Image></View>
-              <View style={[styles.cell, styles.cell12]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(2,3)}></Image></View>
+            <View style={[styles.cell, styles.cell9]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(2,0, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell10]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(2,1, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell11]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(2,2, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell12]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(2,3, board, theme)}></Image></View>
             </View>
             <View>
-            <View style={[styles.cell, styles.cell13]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(3,0)}></Image></View>
-              <View style={[styles.cell, styles.cell14]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(3,1)}></Image></View>
-              <View style={[styles.cell, styles.cell15]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(3,2)}></Image></View>
-              <View style={[styles.cell, styles.cell16]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(3,3)}></Image></View>
+            <View style={[styles.cell, styles.cell13]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(3,0, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell14]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(3,1, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell15]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(3,2, board, theme)}></Image></View>
+              <View style={[styles.cell, styles.cell16]}><Image resizeMode={'contain'}  style={{ width: "100%", height: "100%" }} source={chooseMonster(3,3, board, theme)}></Image></View>
             </View>
           </View>
         </View>
---
+++ utils.js
@@ -0,0 +1,174 @@
+export const chooseMonster = (x,y, board, theme) => {
+    console.log(board[x][y])
+    if(board[x][y] == ""){
+      return images[theme]['Transparent'].file
+    }
+    if(board[x][y] == 2){
+      return images[theme]['One'].file
+    }
+    if(board[x][y] == 4){
+      return images[theme]['Two'].file
+    }    
+    if(board[x][y] == 8){
+      return images[theme]['Three'].file
+    }
+    if(board[x][y] == 16){
+      return images[theme]['Four'].file
+    }
+    if(board[x][y] == 32){
+      return images[theme]['Five'].file
+    }
+    if(board[x][y] == 64){
+      return images[theme]['Six'].file
+    }
+    if(board[x][y] == 128){
+      return images[theme]['Seven'].file
+    }
+    if(board[x][y] == 256){
+      return images[theme]['Eight'].file
+    }
+    if(board[x][y] == 512){
+      return images[theme]['Nine'].file
+    }
+    if(board[x][y] == 1024){
+      return images[theme]['Ten'].file
+    }
+    if(board[x][y] == 2048){
+      return images[theme]['Eleven'].file
+    }
+    if(board[x][y] == 4096){
+      return images[theme]['Twelve'].file
+    }
+    if(board[x][y] == 8192){
+      return images[theme]['Thirteen'].file
+    }
+    if(board[x][y] == 16384){
+      return images[theme]['Fourteen'].file
+    }
+    if(board[x][y] == 32768){
+      return images[theme]['Fifteen'].file
+    }
+    if(board[x][y] == 65536){
+      return images[theme]['Sixteen'].file
+    }
+    if(board[x][y] == 131072){
+      return images[theme]['Seventeen'].file
+    }
+  }
+
+
+
+export const images = {
+    monsters:{
+      Transparent:{
+        file: require('./assets/monsters/transparent.png')
+      },
+      One: {
+        file: require('./assets/monsters/lvl1.png')
+      },
+      Two:{
+        file: require('./assets/monsters/lvl2.png')
+      },
+      Three:{
+        file: require('./assets/monsters/lvl3.png')
+      },
+      Four:{
+        file: require('./assets/monsters/lvl4.png')
+      },
+      Five:{
+        file: require('./assets/monsters/lvl5.png')
+      },
+      Six:{
+        file: require('./assets/monsters/lvl6.png')
+      },
+      Seven:{
+        file: require('./assets/monsters/lvl7.png')
+      },
+      Eight:{
+        file: require('./assets/monsters/lvl8.png')
+      },
+      Nine:{
+        file: require('./assets/monsters/lvl9.png')
+      },
+      Ten:{
+        file: require('./assets/monsters/lvl10.png')
+      },
+      Eleven:{
+        file: require('./assets/monsters/lvl11.png')
+      },
+      Twelve:{
+        file: require('./assets/monsters/lvl12.png')
+      },
+      Thirteen:{
+        file: require('./assets/monsters/lvl13.png')
+      },
+      Fourteen:{
+        file: require('./assets/monsters/lvl14.png')
+      },
+      Fifteen:{
+        file: require('./assets/monsters/lvl15.png')
+      },
+      Sixteen:{
+        file: require('./assets/monsters/lvl16.png')
+      },
+      Seventeen:{
+        file: require('./assets/monsters/lvl17.png')
+      }
+    },
+    numbers:{
+      Transparent:{
+        file: require('./assets/monsters/transparent.png')
+      },
+      One: {
+        file: require('./assets/numbers/2.png')
+      },
+      Two:{
+        file: require('./assets/numbers/4.png')
+      },
+      Three:{
+        file: require('./assets/numbers/8.png')
+      },
+      Four:{
+        file: require('./assets/numbers/16.png')
+      },
+      Five:{
+        file: require('./assets/numbers/32.png')
+      },
+      Six:{
+        file: require('./assets/numbers/64.png')
+      },
+      Seven:{
+        file: require('./assets/numbers/128.png')
+      },
+      Eight:{
+        file: require('./assets/numbers/256.png')
+      },
+      Nine:{
+        file: require('./assets/numbers/512.png')
+      },
+      Ten:{
+        file: require('./assets/numbers/1024.png')
+      },
+      Eleven:{
+        file: require('./assets/numbers/4096.png')
+      },
+      Twelve:{
+        file: require('./assets/numbers/2048.png')
+      },
+      Thirteen:{
+        file: require('./assets/numbers/8192.png')
+      },
+      Fourteen:{
+        file: require('./assets/numbers/16384.png')
+      },
+      Fifteen:{
+        file: require('./assets/numbers/32768.png')
+      },
+      Sixteen:{
+        file: require('./assets/numbers/65536.png')
+      },
+      Seventeen:{
+        file: require('./assets/numbers/131072.png')
+      }
+    }
+  }
